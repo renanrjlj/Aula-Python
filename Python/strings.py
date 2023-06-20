fb2f63867a21319b3300a64754da1ac0aa85f84c
Renan Teixeira <renanpedroteixeira@gmail.com>
2023-06-20 12:16:22 -0300

    Aula 3 de Python

---
+++ Python/strings.py
@@ -0,0 +1,9 @@
+frase = "Olá, seja bem vindo!"
+
+print("String Original: ");
+print(frase);
+print();
+
+print("Acessando caracteres individuais");
+print("Primeiro Caractere: ", frase[0]);
+print("Último caractere: ", frase[1]);
